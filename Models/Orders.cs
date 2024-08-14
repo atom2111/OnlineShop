1788a25ae2d192073b25c08f8ac8d2d3a9e063af
Atom <58501038+atom2111@users.noreply.github.com>
2024-08-14 21:53:05 +0500

    first deploy

---
+++ Models/Orders.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineShop.Models
+{
+    public class Orders
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal Total { get; set; }
+    }
+}
